63081a4aa4cb0a3722905c1b7c1eb878e8dbdd28
thiagotkd2 <thiagop.pf.moraes@gmail.com>
2024-11-01 15:07:23 -0300

    cpp implemented

--- ordenacao.java
+++ ordenacao.java
@@ -1,20 +1,35 @@
 import java.util.Arrays;
 import java.util.Random;
-public class ordenacao{
-    public static void main(String[] args){
-        int[] arr = new int[5];
-        Random rdm = new Random();
-        for(int i = 0; i<arr.length; i++){
-            arr[i] = rdm.nextInt(50);
-        }
+public class ordenacao {
+	public static void main(String[] args) {
+		// array declaration
+		int[] arr = new int[5];
 
-        System.out.println(Arrays.toString(arr));
+		// random class
+		Random rdm = new Random();
 
-        Arrays.sort(arr);
-        
+		// populating the array
+		for(int i = 0; i<arr.length; i++) {
+			arr[i] = rdm.nextInt(50);
+		}
 
-        System.out.println(Arrays.toString(arr));
+		// print the array
+		System.out.println(Arrays.toString(arr));
+
+		// find the max value
+		int max = arr[0];
+		for(int i = 1; i<4; i++) {
+			if(max<arr[i]) {
+				max=arr[i];
+			}
+		}
+
+		// Arrays.sort(arr); -> alternative way is sorting
+		// then print the last element.
+
+		// then print it
+		System.out.println(max);
 
 
-    }
+	}
 }
