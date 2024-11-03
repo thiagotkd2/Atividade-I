ee3618172a13262edf999fafa0f8c0fefc23c85c
thiagotkd2 <75231461+thiagotkd2@users.noreply.github.com>
2024-11-03 19:28:51 -0300

    Adjusting extra spaces greatest.cpp

--- greatest.cpp
+++ greatest.cpp
@@ -17,17 +17,14 @@ int main(){
     // declaring random seed and array
     std::array<int, 5> arr;
     srand(time(NULL));
-    
     // populate the array
     for(int i =0; i<5; ++i){
         arr[i] = rand()%50;
     }
-    
     // print array
     for (const auto& e : arr) {
         std::cout << e << std::endl;
     }
-    
     // find the max
     int max = arr[0];
     for(int i =1; i<5;i++){
@@ -35,14 +32,10 @@ int main(){
             max = arr[i];
         }
     }
-    
     // then, the max value is:
     std::cout<<"O valor max é: "<<max;
-    
     // alternatively, we can get the max value by:
     // int maxNoLoop = *std::max_element(arr.begin(), arr.end());
     // std::cout << "O valor max é:  " << maxElement << std::endl;
-    
-    
     return 0;
 }
