305a7a76b1871880b4619f4aafdc6dfd77d8d882
thiagotkd2 <75231461+thiagotkd2@users.noreply.github.com>
2024-11-06 19:49:52 -0300

    Update greatest.cpp

--- greatest.cpp
+++ greatest.cpp
@@ -3,7 +3,7 @@
 int main(){
     std::array<int, 7> arr = {0, 20, 1, 0, 16, 1, 10};
     int max = arr[0];
-    for(int i =1; i<5;i++){
+    for(int i =1; i<arr.size();i++){
         if(max<arr[i]){
             max = arr[i];
         }
