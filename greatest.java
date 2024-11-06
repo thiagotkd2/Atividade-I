380a9159ac80a8f470f5032517a4bc5a7270d82f
thiagotkd2 <75231461+thiagotkd2@users.noreply.github.com>
2024-11-06 19:31:07 -0300

    demonstração compactada java

--- greatest.java
+++ greatest.java
@@ -1,27 +1,12 @@
-import java.util.Arrays;
-import java.util.Random;
 public class greatest {
 	public static void main(String[] args) {
-		// array declaration
-		int[] arr = new int[5];
-		// random class
-		Random rdm = new Random();
-		// populating the array
-		for(int i = 0; i<arr.length; i++) {
-			arr[i] = rdm.nextInt(50);
-		}
-		// print the array
-		System.out.println(Arrays.toString(arr));
-		// find the max value
+		int[] arr = {0, 20, 1, 0, 16, 1, 10};
 		int max = arr[0];
 		for(int i = 1; i<4; i++) {
 			if(max<arr[i]) {
 				max=arr[i];
 			}
 		}
-		// Arrays.sort(arr); -> alternative way is sorting
-		// then print the last element.
-		// then print it
 		System.out.println(max);
 	}
 }
